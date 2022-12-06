a63fd77ef7f702b5a8b216807fc18b077029ce56
joshua_crompton <josh.m.crompton@gmail.com>
2022-12-06 02:02:26 +0000

    Day 1 naïve solution

---
+++ Day1/Calories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Calories
+    {
+        public List<int> CalorieList { get; } = new List<int>();
+
+        public int GetTotal()
+        {
+            return CalorieList.Sum();
+        }
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -2,9 +2,51 @@
 {
     internal class Program
     {
+        private static List<Calories> _calories = new List<Calories>();
+
+        static void ProcessCalories( string input )
+        {
+            string[] splitStrings = input.Split("\n");
+            Calories calories = new Calories();
+            foreach( string split in splitStrings )
+            {
+                if( split.Trim().Length == 0)
+                {
+                    _calories.Add( calories );
+                    calories = new Calories();
+                }
+                else
+                {
+                    calories.CalorieList.Add(Int32.Parse(split));
+                }
+            }
+            if( calories.CalorieList.Count > 0 )
+            {
+                _calories.Add(calories);
+            }
+
+            _calories.Sort(delegate (Calories x, Calories y)
+            {
+                int xCalories = x.GetTotal();
+                int yCalories = y.GetTotal();
+                return yCalories - xCalories;
+            });
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = System.IO.File.ReadAllText("Data/input.txt");
+            ProcessCalories(text);
+            int maxCalories = _calories.Max(x => x.GetTotal());
+            Console.WriteLine( $"Max Calories: {maxCalories}");
+            int topThree = 0;
+            foreach (var i in Enumerable.Range(0, 3))
+            {
+                topThree += _calories[i].GetTotal();
+            }
+            Console.WriteLine($"Top 3 Calories: {topThree}");
+
         }
     }
 }
